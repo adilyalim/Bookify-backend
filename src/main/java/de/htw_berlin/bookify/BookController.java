8544065a0e2cd7f4116e30ad0a92b2f36eedb0a1
Munise Yalim <Munise.Yalim@Student.HTW-Berlin.de>
2026-05-24 16:30:18 +0200

    Add CORS

--- src/main/java/de/htw_berlin/bookify/BookController.java
+++ src/main/java/de/htw_berlin/bookify/BookController.java
@@ -1,10 +1,12 @@
 package de.htw_berlin.bookify;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class BookController {
 
 
